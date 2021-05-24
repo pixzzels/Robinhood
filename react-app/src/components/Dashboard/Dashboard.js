f66964b5e2aa09dc43948bb717ac6b16f012f4aa
Ellen Park <paarkellen@gmail.com>
2021-05-24 13:43:18 -0700

    created dashboard page

---
+++ react-app/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+
+import './Dashboard.css';
+
+
+function Dashboard() {
+    return (
+        <div className="dashboard-container">
+            Hello Dashboard
+        </div>
+    )
+}
+
+export default Dashboard;
