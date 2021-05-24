b078fdb0f724ea7c0888ba0b4cfcd1d1b83e1c67
Eunice Park <euunicepaark@gmail.com>
2021-05-24 13:42:55 -0400

    set up routes and components for splash page

---
+++ react-app/src/components/SplashPage/index.js
@@ -0,0 +1,11 @@
+import './SplashPage.css'
+
+function SplashPage() {
+  return (
+    <div className='splash-wrapper'>
+      <h2>hello splashpage</h2>
+    </div >
+  )
+}
+
+export default SplashPage
