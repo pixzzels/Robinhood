fd61239c410091fada7f2ded1f99ea3337c15a42
Eunice Park <euunicepaark@gmail.com>
2021-05-24 15:58:56 -0400

    added links and img

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -6,6 +6,8 @@ import './NavBar.css';
 
 const NavBar = () => {
   const user = useSelector(state => state.session.user);
+  const logo = require("../../images/logo-black.png")
+
 
   const logoutLink = (
     <>
@@ -20,22 +22,32 @@ const NavBar = () => {
     <nav className="nav-wrapper">
       <div className="nav-link" id="home-link">
         <NavLink to="/" exact={true} activeClassName="active">
-          Home
+          <img src={logo} alt='logo'></img>
         </NavLink>
       </div>
+
+      {/* links to profile ? drop down ??? */}
+      <div className="nav-link" id="contact-link">
+        <NavLink to="/contactus" exact={true} activeClassName="active">
+          Contact us
+        </NavLink>
+      </div>
+
+      <div className="nav-link" id="about-link">
+        <NavLink to="/whoweare" exact={true} activeClassName="active">
+          Who we are
+        </NavLink>
+      </div>
+
       <div className="nav-link" id="login-link">
         <NavLink to="/login" exact={true} activeClassName="active">
           Login
         </NavLink>
       </div>
+
       <div className="nav-link" id="signup-link">
         <NavLink to="/sign-up" exact={true} activeClassName="active">
           Sign Up
-        </NavLink>
-      </div>
-      <div className="nav-link" id="home-link">
-        <NavLink to="/whoweare" exact={true} activeClassName="active">
-          Who we are
         </NavLink>
       </div>
       <div>
