b906dda66999aae87065dee3080ab06296220fdf
schuler <schuler.small.10@gmail.com>
2021-05-24 18:35:58 -0500

    forgot to commit earlier, fixed the trade gif and added more content to splash

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './SplashPage.css';
 import trade from '../../images/trade.gif';
 
@@ -9,13 +10,17 @@ function SplashPage() {
         <div id="message-wrapper">
           <div id="c1-message1">Investing for Everyone</div>
           <div id="c1-message2">Commission-free investing, plus the tools you need to put your money in motion. Sign up and get your first stock for free. Certain limitations apply.</div>
-          <div>
-            Sign Up
-          </div>
+          <NavLink to="/sign-up" exact={true} id="splash-signup__link" activeClassName="active">
+              Sign Up
+          </NavLink>
         </div>
         <div id="trade-gif_wrapper">
-          {/* <img src={trade} alt='trade gif' id='trade'></img> */}
+          <img src={trade} alt='trade gif' id='trade'></img>
         </div>
+      </div>
+
+      <div id="container-2">
+        We will never charge you commissions...or make you any money for that matter.
       </div>
     </div >
   )
