74a543230fe8b260e3f29635d84698c85fe04539
Ellen Park <paarkellen@gmail.com>
2021-05-24 15:18:25 -0700

    added more styling and outline to dashboard

--- react-app/src/components/Dashboard/Dashboard.js
+++ react-app/src/components/Dashboard/Dashboard.js
@@ -8,12 +8,83 @@ function Dashboard() {
         <>
             <div className="dashboard-container">
                 {/* <h1>Hello Dashboard</h1> */}
-                <div className="portfolio-container">
-                    <div className="total-investment">
-                        $400.34
-                </div>
-                </div>
-                <div className="watchlist-container">
+                <div className="div-row">
+
+
+                    <div className="portfolio-container">
+                        <div className="total-investment">
+                            $400.34
+                        </div>
+                        <div className="portfolio-timeline">
+                            | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
+                            | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
+                            | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
+                            | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
+                            | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
+                            | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
+                            | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
+                            | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
+                            | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
+                            | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
+                            | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
+                            | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
+                            | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
+
+                        </div>
+                        <div className="portfolio-timeline-bar">
+                            <button className="portfolio-timeline-options btn">1D</button>
+                            <button className="portfolio-timeline-options btn">1W</button>
+                            <button className="portfolio-timeline-options btn">1M</button>
+                            <button className="portfolio-timeline-options btn">1Y</button>
+                            <button className="portfolio-timeline-options btn">ALL</button>
+                        </div>
+                        <div className="buying-power">
+                            <span>Buying Power</span>
+                            <span>$3034.76</span>
+                        </div>
+                    </div>
+
+
+                    <div className="watchlist-container">
+                        <div className="all-stocks-container">
+
+                            <div className="all-stocks-header">
+                                <span>Stocks</span>
+                                <button className="all-stocks-header-btn btn">
+                                    <svg fill="none" height="24" role="img" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M6.5 10.5H3.5V13.5H6.5V10.5Z" fill="grey"></path>
+                                        <path d="M13.5 10.5H10.5V13.5H13.5V10.5Z" fill="grey"></path>
+                                        <path d="M17.5 10.5H20.5V13.5H17.5V10.5Z" fill="grey"></path>
+                                    </svg>
+                                </button>
+                            </div>
+
+                            <div className="all-stock__stock">
+                                <div className="stock-name-shares-owned">
+                                    <span>APPL</span>
+                                    <span>3 Shares</span>
+                                </div>
+                                <div className="all-stock__graph-container">
+                                    :)
+                                </div>
+                                <div className="all-stock_current-price">
+                                    <span>$127.02</span>
+                                    <span>-3.29%</span>
+                                </div>
+                            </div>
+
+                            <div className="all-stocks-header">
+                                <span>Lists</span>
+                                <button className="btn" type="button">
+                                    <svg fill="none" height="16" role="img" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M7.125 8.875V14H8.875V8.875H14V7.125H8.875V2H7.125V7.125H2V8.875H7.125Z" fill="black"></path>
+                                    </svg>
+                                </button>
+                            </div>
+
+                        </div>
+                    </div>
+
                 </div>
             </div>
             <button>
