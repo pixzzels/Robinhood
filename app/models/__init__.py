012a174bcb4c4ecdfea254e5b2b338892af06804
Ellen Park <paarkellen@gmail.com>
2021-05-24 13:07:39 -0700

    generated tables

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,9 @@
 from .db import db
 from .user import User
 from .portfolio import Portfolio
+from .stock import Stock
+from .transaction import Transaction
+from .watchlist import Watchlist
+from .list import List
+
+
