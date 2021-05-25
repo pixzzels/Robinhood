f4e93ae3a18c21281a9bd673456d2b97eab10e0f
Ellen Park <paarkellen@gmail.com>
2021-05-25 09:12:11 -0700

    added deposit funds form

--- react-app/src/components/Dashboard/Dashboard.js
+++ react-app/src/components/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ReactDOM from "react-dom";
 import { Collapse } from "reactstrap";
+import { Modal } from '../../context/Modal';
 
 
 import './Dashboard.css';
@@ -10,6 +11,8 @@ function Dashboard() {
     const [listForm, setListForm] = useState(false);
     const [newList, setNewList] = useState('');
     const [bpDivExpand, setbpDivExpand] = useState(false);
+    const [fundsForm, setFundsForm] = useState(false);
+    const [showDepositModal, setShowDepositModal] = useState(false);
 
     const addList = () => {
         setListForm(!listForm)
@@ -20,9 +23,18 @@ function Dashboard() {
     }
 
     const bpLearnMore = () => {
-        
+
     }
 
+    const showDepositForm = () => {
+        setShowDepositModal(!showDepositModal)
+    }
+
+    const depositFunds = () => {
+        setShowDepositModal(!showDepositModal)
+    }
+
+    
     return (
         <>
             <div className="dashboard-container">
@@ -58,36 +70,63 @@ function Dashboard() {
                             <button className="portfolio-timeline-options btn tlbtn">ALL</button>
 
                         </div>
-                        <button className="buying-power-container-btn" type="button" onClick={buyingPowerExpand}>
+                        <button className={"buying-power-container-btn " + (bpDivExpand ? 'grey' : '')} type="button" onClick={buyingPowerExpand}>
                             <div className={"buying-power-header " + (bpDivExpand ? 'grey' : '')}>
                                 <span>Buying Power</span>
                                 {/* <span>$3034.76</span> */}
                                 <span className={"buying-power-number " + (bpDivExpand ? 'hidden' : '')}>$3034.76</span>
                             </div>
                         </button>
-                            {bpDivExpand &&
-                                <div className="buying-power-add grey">
-                                    <div className="buying-power-mini-container">
-                                        <div className="buying-power__2">
-                                            <span>Brokerage Power</span>
-                                            <span>$3034.76</span>
-                                        </div>
-                                        <div className="buying-power__2">
-                                            <span>Buying Power</span>
-                                            <span>$3034.76</span>
-                                        </div>
+                        {bpDivExpand &&
+                            <div className="buying-power-add grey">
+                                <div className="buying-power-mini-container">
+                                    <div className="buying-power__2">
+                                        <span>Brokerage Power</span>
+                                        <span>$3034.76</span>
                                     </div>
+                                    <div className="buying-power__2">
+                                        <span>Buying Power</span>
+                                        <span className="bold">$3034.76</span>
+                                    </div>
+                                    <button
+                                        type="button"
+                                        className="deposit-funds-btn"
+                                        onClick={showDepositForm}
+                                    >
+                                        Deposit Funds
+                                        </button>
+                                    {showDepositModal &&
+                                        (<Modal onClose={() => setShowDepositModal(false)}>
+                                            <h2>Deposit Funds</h2>
+                                            <form className="deposit-funds-form">
+                                                <label htmlFor="amount">
+                                                    Amount
+                                                </label>
+                                                <input 
+                                                type="text"
+                                                name="amount"
+                                                >
+                                                </input>
+                                                <button
+                                                type="submit"
+                                                onClick={depositFunds}>
+                                                    Deposit
+                                                </button>
+                                            </form>
+                                        </Modal>)
+                                    }
+                                </div>
 
-                                    <div className="buying-power__info">
-                                        Buying Power represents the total value of stocks you can purchase.
-                                        <button 
+                                <div className="buying-power__info">
+                                    Buying Power represents the total value of stocks you can purchase.
+                                        <button
                                         className="buying-power__learn-more-btn"
                                         type="button"
                                         onClick={bpLearnMore}
-                                        >Learn More</button>
-                                    </div>
+                                    >Learn More</button>
                                 </div>
-                            }
+                            </div>
+                        }
                     </div>
 
                     <div className="watchlist-container">
