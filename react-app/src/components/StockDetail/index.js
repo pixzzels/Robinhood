ffe9027eb0206149709da458e4a2eb6fc86411c2
schuler <schuler.small.10@gmail.com>
2021-05-26 15:56:50 -0500

    added news stories to stock detail

--- react-app/src/components/StockDetail/index.js
+++ react-app/src/components/StockDetail/index.js
@@ -72,6 +72,21 @@ function StockDetail() {
                             </div>
                         </div>
                     </div>
+
+                    <div className="news-container">
+                        <div id="news-title">News</div>
+                        <hr id="hr"></hr>
+                        <a href="story-link-here">
+                            <div className="news-story">
+                                <div id="story-origin">Origin</div>
+                                <div id="story-title">Story Title</div>
+                                <div id="story-content">An Apple Store Employee...</div>
+                                <div id="story-image">
+                                    <img src="" id="story-img"></img>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
                 </div>
 
                 <div className="buy-sell__wrapper">
