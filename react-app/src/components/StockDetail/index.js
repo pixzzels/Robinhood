eaa1c6edea48ac49ce46856c8a3cd38edab036be
schuler <schuler.small.10@gmail.com>
2021-05-26 13:39:20 -0500

    more adds to stock detail page

--- react-app/src/components/StockDetail/index.js
+++ react-app/src/components/StockDetail/index.js
@@ -32,14 +32,45 @@ function StockDetail() {
                         <div id="about">About</div>
                         <hr id="hr"></hr>
                         <div id="description">
-                            Company Description
+                            Apple, Inc. engages in the design, manufacture, and sale of smartphones, personal computers, tablets, wearables and accessories, and other variety of related services. It operates through the following geographical segments: Americas, Europe, Greater China, Japan, and Rest of Asia Pacific. 
                         </div>
-                        <div id="company-info">CEO, Employees....</div>
+                        <div className="company-info-container">
+                            <div>
+                                <div className="info-label">CEO</div>
+                                <div>Timothy Donald Cook</div>
+                            </div>
+                            <div>
+                                <div className="info-label">Employees</div>
+                                <div>147,000</div>
+                            </div>
+                            <div>
+                                <div className="info-label">Headquarters</div>
+                                <div>Cupertino, California</div>
+                            </div>
+                        </div>
                     </div>
 
                     <div className="statistics-container">
                         <div id="statistics-title">Key Statistics</div>
-                        <div id="statistics">Market Cap, Price-Earnings Ratio etc</div>
+                        <hr id="hr"></hr>
+                        <div id="statistics">
+                            <div>
+                                <div className="info-label">Market Cap</div>
+                                <div>2.14T</div>
+                            </div>
+                            <div>
+                                <div className="info-label">Price-Earnings Ratio</div>
+                                <div>28.48</div>
+                            </div>
+                            <div>
+                                <div className="info-label">Dividend Yield</div>
+                                <div>0.65</div>
+                            </div>
+                            <div>
+                                <div className="info-label">Average Volume</div>
+                                <div>94.75M</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
