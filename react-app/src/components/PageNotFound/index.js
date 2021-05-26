ff6ac372087deb1bc1e392813de5750909b662e1
schuler <schuler.small.10@gmail.com>
2021-05-26 10:40:26 -0500

    fixed nav logout button issue

--- react-app/src/components/PageNotFound/index.js
+++ react-app/src/components/PageNotFound/index.js
@@ -1,21 +1,25 @@
 import { NavLink } from "react-router-dom";
-import "./PageNotFound.css"
-import errorImg from "../../images/error-img.png"
+import NavBar from "../NavBar/index";
+import "./PageNotFound.css";
+import errorImg from "../../images/error-img.png";
 
 const PageNotFound = () => {
   return (
-    <div className="error-container">
-      <div className="error-text__container">
-        <h2 className='error-header1'>404</h2>
-        <h2 className='error-header2'>Page Not Found</h2>
-        <h4 id='error-txt'>Not all those who wander are lost, but it seems you may have taken a wrong turn.</h4>
-        <NavLink to="/" className='error-link'>Go Home</NavLink>
-      </div>
+    <>
+      <NavBar />
+      <div className="error-container">
+        <div className="error-text__container">
+          <h2 className='error-header1'>404</h2>
+          <h2 className='error-header2'>Page Not Found</h2>
+          <h4 id='error-txt'>Not all those who wander are lost, but it seems you may have taken a wrong turn.</h4>
+          <NavLink to="/" className='error-link'>Go Home</NavLink>
+        </div>
 
-      <div className='error-image__container'>
-        <img alt='error image' className='error-image' src={errorImg}></img>
+        <div className='error-image__container'>
+          <img alt='error image' className='error-image' src={errorImg}></img>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
