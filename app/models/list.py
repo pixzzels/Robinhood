76a9c433799bdfb111e234e677eadcca3bb10067
Jack Radinger <jackradinger98@gmail.com>
2021-05-26 23:43:00 -0500

    changes

--- app/models/list.py
+++ app/models/list.py
@@ -4,8 +4,8 @@ class List(db.Model):
     __tablename__ = 'Lists'
 
     id = db.Column(db.Integer, primary_key = True)
-    watchlist_id = db.Column(db.Integer, db.ForeignKey('Watchlists.id'), nullable = False, unique = True)
-    stock_id = db.Column(db.Integer, db.ForeignKey('Stocks.id'), nullable = False, unique = True)
+    watchlist_id = db.Column(db.Integer, db.ForeignKey('Watchlists.id'), nullable = False)
+    stock_id = db.Column(db.Integer, db.ForeignKey('Stocks.id'), nullable = False)
 
     watchlist = db.relationship('Watchlist')
     stock = db.relationship('Stock')
