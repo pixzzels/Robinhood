e6eac16513f92c4461e4800d12abf93120d14536
Eunice Park <euunicepaark@gmail.com>
2021-05-27 18:09:22 -0400

    finished account dropdown

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
@@ -7,6 +7,13 @@ import blackleaf from '../../images/robinhood-2.svg';
 
 const NavBar = () => {
   const user = useSelector(state => state.session.user);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  console.log('user', user)
+
+  const handleDropdown = () => {
+    setShowDropdown(!showDropdown)
+  }
 
   return (
     <nav className="nav-wrapper">
@@ -36,14 +43,45 @@ const NavBar = () => {
         </div>
 
         <div className="portfolio-nav__links">
-          <NavLink to="/account" exact={true} id="account-link" activeClassName="active">
-            Account
-          </NavLink>
+          <a href="https://github.com/pixzzels/Robinhood" target="_blank" id="github-link" activeClassName="active">
+            Our GitHub Repository
+          </a>
         </div>
 
-        <>
-          <LogoutButton />
-        </>
+
+        <div className="portfolio-nav__links">
+          <button type="button" onClick={handleDropdown} id="account-link">
+            Account
+          </button>
+        </div>
+        {showDropdown &&
+          <div className="account-options-dropdown">
+
+            <div className="drop-container1">
+              <div id="users-name">{user.first_name} {user.last_name}</div>
+              <div>{user.email}</div>
+            </div>
+
+            <div className="drop-container2">
+              <NavLink to="/account" exact={true} activeClassName="active" className="account-list history-link">
+                <i class="fas fa-toolbox drop-icon"></i>
+                Account
+              </NavLink>
+
+              <NavLink to="/history" exact={true} activeClassName="active" className="account-list history-link">
+                <i class="fas fa-history drop-icon"></i>
+                History
+               </NavLink>
+            </div>
+
+            <div className="drop-container3 account-list">
+              <i class="fas fa-sign-out-alt drop-icon"></i>
+              <LogoutButton />
+            </div>
+
+          </div>
+        }
+
       </div>
 
     </nav>
