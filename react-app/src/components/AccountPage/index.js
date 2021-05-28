f406638ab3cc60ed207ec2a7b7aa61aadec4636d
Ellen Park <paarkellen@gmail.com>
2021-05-28 15:28:30 -0400

    finished transaction history page

--- react-app/src/components/AccountPage/index.js
+++ react-app/src/components/AccountPage/index.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AccountHistory from '../AccountHistory'
 import AccountPageNavBar from '../AccountPageNavBar'
 
 import './AccountPage.css'
 
 function AccountPage() {
+    const [content, setContent] = useState('')
+    
+    let component;
+    const changeContent = (type) => {
+        if (type === "history") {
+            // setContent("history")
+            component = (<AccountHistory/>)
+        }
+    }
+
 
     return (
         <>
-            <AccountPageNavBar />
-            <div className="account-page-wrapper">hey?werwerw</div>
+            <AccountPageNavBar changeContent={changeContent} />
+            <div className="account-page-wrapper">
+                <AccountHistory />
+            </div>
 
         </>
     )
