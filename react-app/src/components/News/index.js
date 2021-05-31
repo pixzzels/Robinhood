8e927a6882425b9ed892b6bc812100742b7d5423
schuler <schuler.small.10@gmail.com>
2021-05-31 10:07:35 -0500

    fixing alt tags

--- react-app/src/components/News/index.js
+++ react-app/src/components/News/index.js
@@ -17,7 +17,7 @@ function NewsStory({news, symbol}) {
                                     <div id="story-content">{story.summary.slice(0, 140)}...</div>
                                 </div>
                                 <div id="story-image">
-                                    <img src={story.image} id="story-img"></img>
+                                    <img src={story.image} alt='news story' id="story-img"></img>
                                 </div>
                             </div>
                         </a>
--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -57,7 +57,7 @@ const SignUpForm = () => {
   return (
     <div className='form-wrapper'>
       <div className='form-image__container'>
-        <img alt='login image' className='form-image' src={formImg}></img>
+        <img alt='login' className='form-image' src={formImg}></img>
       </div>
 
       <form className='form-container' onSubmit={onSignUp}>
