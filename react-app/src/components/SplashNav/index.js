c7a4aab817499102038fdb92a6703c768b516a94
schuler <schuler.small.10@gmail.com>
2021-05-31 10:17:08 -0500

    removed active classes and warnings

--- react-app/src/components/SplashNav/index.js
+++ react-app/src/components/SplashNav/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import LogoutButton from '../auth/LogoutButton';
+// import LogoutButton from '../auth/LogoutButton';
 import './SplashNav.css';
 // import logo from '../../images/logo-black-small.png';
 import logo from '../../images/robinhood-vector-logo.png';
@@ -21,32 +21,32 @@ const NavBar = () => {
   return (
     <nav className="nav-wrapper">
       <div className="nav-link" id="home-link">
-        <NavLink to="/" exact={true} activeClassName="active">
+        <NavLink to="/" exact={true}>
           <img src={logo} alt='logo' id='logo-black'></img>
         </NavLink>
       </div>
 
       {/* links to profile ? drop down ??? */}
       <div className="nav-link greens" id="contact-link">
-        <a href="https://github.com/pixzzels/Robinhood" rel="noopener noreferrer" target="_blank" exact={true} activeClassName="active">
+        <a href="https://github.com/pixzzels/Robinhood" rel="noopener noreferrer" target="_blank" exact={true}>
           Check Out Our Work
         </a>
       </div>
 
       <div className="nav-link greens" id="about-link">
-        <a href="#footer-container" exact={true} activeClassName="active">
+        <a href="#footer-container" exact={true}>
           Who we are
         </a>
       </div>
 
       <div className="nav-link greens" id="login-link">
-        <NavLink to="/login" exact={true} activeClassName="active">
+        <NavLink to="/login" exact={true}>
           Log In
         </NavLink>
       </div>
 
       <div className="nav-link" id="signup-link">
-        <NavLink to="/sign-up" exact={true} activeClassName="active">
+        <NavLink to="/sign-up" exact={true}>
           Sign Up
         </NavLink>
       </div>
