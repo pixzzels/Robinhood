9e55ab6ba3fbfcec72ce4e4aedb9c94c0846c57b
Ellen Park <paarkellen@gmail.com>
2021-05-31 23:19:26 -0400

    added seed portfolio data

--- app/seeds/portfolios.py
+++ app/seeds/portfolios.py
@@ -8,7 +8,24 @@ def seed_portfolios():
     demo_portfolio = Portfolio(
         user_id=1, cash_balance=50000, total_investment=34000, portfolio_stats=[1, 2, 3])
 
+    ellen_portfolio = Portfolio(
+        user_id=2, cash_balance=50000, total_investment=34000, portfolio_stats=[1, 2, 3])
+
+    eunice_portfolio = Portfolio(
+        user_id=3, cash_balance=50000, total_investment=34000, portfolio_stats=[1, 2, 3])
+
+    schuler_portfolio = Portfolio(
+        user_id=4, cash_balance=50000, total_investment=34000, portfolio_stats=[1, 2, 3])
+
+    jack_portfolio = Portfolio(
+        user_id=5, cash_balance=50000, total_investment=34000, portfolio_stats=[1, 2, 3])
+
     db.session.add(demo_portfolio)
+    db.session.add(ellen_portfolio)
+    db.session.add(eunice_portfolio)
+    db.session.add(schuler_portfolio)
+    db.session.add(jack_portfolio)
+
     db.session.commit()
 # Uses a raw SQL query to TRUNCATE the users table.
 # SQLAlchemy doesn't have a built in function to do this
